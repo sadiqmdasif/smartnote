cfa6f97c5f596650438904fd1aa5a41d65e975b0
sadiqmdasif <asifsadiqmd@gmail.com>
2021-11-08 20:49:11 +0600

    post profile

--- lib/core/screen/profile/proifle.dart
+++ lib/core/screen/profile/proifle.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:smartnote/core/screen/profile/profile_about.dart';
+import 'package:smartnote/core/screen/profile/profile_post.dart';
 import 'package:smartnote/utils/theme/theme.dart';
 
 class Profile extends StatefulWidget {
@@ -28,8 +30,9 @@ class _ProfileState extends State<Profile> {
         ),
         body: SingleChildScrollView(
           child: Container(
-              color: Colors.white,
-              child: Column(children: [
+            color: Colors.white,
+            child: Column(
+              children: [
                 Container(
                     height: 20,
                     width: 900,
@@ -80,7 +83,7 @@ class _ProfileState extends State<Profile> {
                                 ),
                               ),
                               SizedBox(
-                                width: 100,
+                                width: 50,
                               ),
                               Text("244 friends",
                                   style: TextStyle(
@@ -150,235 +153,21 @@ class _ProfileState extends State<Profile> {
                             ]),
                       ),
                       Container(
-                        height: MediaQuery.of(context).size.height * 1,
+                        height: MediaQuery.of(context).size.height * 1.3,
                         color: Colors.white,
                         child: TabBarView(
                           children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(12.0),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceAround,
-                                    children: [
-                                      Column(
-                                        children: [
-                                          Text(
-                                            "VIEW",
-                                            style: TextStyle(
-                                                fontSize: 12,
-                                                color: Colors.grey.shade500),
-                                          ),
-                                          Text(
-                                            "10",
-                                            style: TextStyle(
-                                                fontSize: 20,
-                                                color: Colors.grey.shade500),
-                                          ),
-                                          Text(
-                                            "View",
-                                            style: TextStyle(
-                                                fontSize: 12,
-                                                color: Colors.grey.shade500),
-                                          ),
-                                        ],
-                                      ),
-                                      Column(
-                                        children: [
-                                          Text(
-                                            "17k RATINGS",
-                                            style: TextStyle(
-                                                fontSize: 12,
-                                                color: Colors.grey.shade500),
-                                          ),
-                                          Text(
-                                            "4.7",
-                                            style: TextStyle(
-                                                fontSize: 20,
-                                                color: Colors.grey.shade500),
-                                          ),
-                                          RatingBar.builder(
-                                            initialRating: 4,
-                                            itemSize: 14,
-                                            minRating: 1,
-                                            direction: Axis.horizontal,
-                                            allowHalfRating: true,
-                                            itemCount: 5,
-                                            itemPadding: EdgeInsets.symmetric(
-                                                horizontal: 2.0, vertical: 2),
-                                            itemBuilder: (context, _) => Icon(
-                                              Icons.star,
-                                              size: 10,
-                                              color: CustomColors.primary,
-                                            ),
-                                            onRatingUpdate: (rating) {
-                                              print(rating);
-                                            },
-                                          ),
-                                        ],
-                                      ),
-                                      Column(
-                                        children: [
-                                          Text(
-                                            "View",
-                                            style: TextStyle(
-                                                fontSize: 12,
-                                                color: Colors.grey.shade500),
-                                          ),
-                                          Text(
-                                            "10",
-                                            style: TextStyle(
-                                                fontSize: 20,
-                                                color: Colors.grey.shade500),
-                                          ),
-                                          Text(
-                                            "View",
-                                            style: TextStyle(
-                                                fontSize: 12,
-                                                color: Colors.grey.shade500),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 20,
-                                ),
-                                Video_card_widegt(),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 20.0, bottom: 10),
-                                  child: Text("About"),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 20.0, bottom: 10),
-                                  child: Text("About gtext"),
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                          left: 20.0, right: 20),
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text("work"),
-                                          Icon(Icons.work),
-                                        ],
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                          left: 40.0, bottom: 8),
-                                      child: Text("Prime tech"),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                          left: 20.0, right: 20),
-                                      child: Divider(
-                                          thickness: 1,
-                                          height: 1,
-                                          color: Colors.grey.shade400),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                          left: 40.0, bottom: 8),
-                                      child: Text("Prime tech"),
-                                    ),
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: 20,
-                                ),
-                                SizedBox(
-                                  height: 20,
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                          left: 20.0, right: 20),
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text("Educations"),
-                                          Icon(Icons.cast_for_education),
-                                        ],
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                          left: 40.0, bottom: 8),
-                                      child: Text("AIub"),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                          left: 20.0, right: 20),
-                                      child: Divider(
-                                          thickness: 1,
-                                          height: 1,
-                                          color: Colors.grey.shade400),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                          left: 40.0, bottom: 8),
-                                      child: Text("Aiub"),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            Text("tab2")
+                            ProfileAboutTab(),
+                            ProfilePostTab(),
                           ],
                         ),
                       ),
                     ],
                   ),
-                )
-              ])),
-        ));
-  }
-
-  Widget Video_card_widegt() {
-    return SingleChildScrollView(
-      child: Container(
-        height: MediaQuery.of(context).size.height * .5,
-        color: Colors.white,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: 5,
-          itemBuilder: (context, int index) {
-            return Padding(
-              padding: const EdgeInsets.only(left: 16.0, bottom: 8, top: 8),
-              child: GestureDetector(
-                onTap: () {},
-                child: Container(
-                  height: MediaQuery.of(context).size.height * .5,
-                  width: MediaQuery.of(context).size.width * .7,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(16),
-                      ),
-                      image: DecorationImage(
-                        image: AssetImage(
-                          "assets/image/videoimage.png",
-                        ),
-                      )),
                 ),
-              ),
-            );
-          },
-        ),
-      ),
-    );
+              ],
+            ),
+          ),
+        ));
   }
 }
